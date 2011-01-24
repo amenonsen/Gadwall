e004fb62d932663566777adca2a082be802e1cf3
Abhijit Menon-Sen <ams@toroid.org>
2011-01-24 11:27:21 +0530

    Add helper functions for JSON-returning actions
    json_error returns an error using whatever information is present in the
    stash or its command-line arguments. json_ok returns a success message.

--- lib/Gadwall/Controller.pm
+++ lib/Gadwall/Controller.pm
@@ -5,4 +5,49 @@ use warnings;
 
 use base 'Mojolicious::Controller';
 
+sub json_fragment {
+    return shift->render(json => { @_ })
+}
+
+# return $self->json_ok("Success!", extra => "data")
+# => {"status": "ok", "message": "Success!", "extra": "data"}
+
+sub json_ok {
+    my $self = shift;
+    return $self->json_fragment(
+        status => 'ok', message => shift, @_
+    );
+}
+
+# return $self->json_error("Failed!")
+# => {"status": "error", "message": "Failed!"}
+#
+# return $self->json_error("Failed!", extra => "data")
+# OR:
+# $self->stash(error => {message => "Failed!", extra => "data"});
+# return $self->json_error
+# OR:
+# return $self->json_error({message => "Failed!", extra => "data"})
+# => {"status": "error", "message": "Failed!", "extra": "data"}
+
+sub json_error {
+    my ($self, $error) = @_;
+
+    my %error;
+
+    $error ||= $self->stash('error');
+    if (ref $error) {
+        %error = %{ $error };
+    }
+    else {
+        %error = (
+            message => $error || "Unknown error", @_
+        );
+    }
+
+    return $self->json_fragment(
+        status => 'error', %error
+    );
+}
+
 1;
