eb64feabe08e9254868429d2f627dce31fa233ee
Abhijit Menon-Sen <ams@toroid.org>
2011-12-11 08:52:25 +0530

    Callbacks can't use render_plaintext

--- lib/App.pm
+++ lib/App.pm
@@ -19,7 +19,9 @@ sub startup {
     my $r = $app->routes;
 
     $r->any(
-        '/' => sub { shift->render_plaintext("Hello world!") }
+        '/' => sub {
+            shift->render_text("Hello world!", format => 'txt')
+        }
     );
 }
 
