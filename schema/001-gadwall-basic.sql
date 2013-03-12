e3e8865b72e02e1085f30a8d2df05d3dd6eb6903
Abhijit Menon-Sen <ams@toroid.org>
2013-03-12 18:50:15 +0530

    Might as well add a users.name column now

--- schema/001-gadwall-basic.sql
+++ schema/001-gadwall-basic.sql
@@ -4,6 +4,7 @@ create table users (
     user_id serial primary key,
     login text unique,
     email text not null unique,
+    name text,
     password text,
     is_active bool not null default true,
     roles bit(31) not null default B'0'::bit(31),
