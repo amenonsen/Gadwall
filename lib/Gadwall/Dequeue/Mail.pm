d355e12da7536ea0d92492890e8f6359f4f16663
Abhijit Menon-Sen <ams@toroid.org>
2013-07-07 21:59:54 +0530

    A very simple implementation of Dequeue::Mail
    This assumes that, since the job has been submitted using enqueue_mail
    and passed _prepare_mail, it is still fine to be passed on to mail().
    That's good enough for now.
    
    In any case, we don't know if we'll continue to use MIME::Lite in the
    future. The module is not maintained or recommended (but it sure is a
    convenient alternative to the current recommendations!).

--- lib/Gadwall/Dequeue/Mail.pm
+++ lib/Gadwall/Dequeue/Mail.pm
@@ -2,10 +2,19 @@ package Gadwall::Dequeue::Mail;
 
 use Mojo::Base 'Gadwall::Dequeue::Base';
 
+use Gadwall::Util 'mail';
+
 sub process {
     my ($self, $data, $job) = @_;
+    my ($qid, $tag, $json) = @$job{qw/queue_id tag data/};
 
-    ...
+    eval {
+        unless (mail(%$data)) {
+            die $@;
+        }
+    } or do {
+        $self->app->log->error("Mail job #$qid failed: $@");
+    }
 }
 
 1;
