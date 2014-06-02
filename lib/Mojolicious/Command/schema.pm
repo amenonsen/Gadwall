1b070d034d399db0113148df8c69e055178295ca
Abhijit Menon-Sen <ams@toroid.org>
2014-06-02 23:20:57 +0530

    Allow 'schema install' to be run as user postgres

--- lib/Mojolicious/Command/schema.pm
+++ lib/Mojolicious/Command/schema.pm
@@ -35,13 +35,16 @@ sub run {
 sub install {
     my ($self, @args) = @_;
 
+    my $app = $self->app;
+
     my ($database, $owner, $user) =
-        @{$self->app->config}{qw/db_name db_owner db_user/};
+        @{$app->config}{qw/db_name db_owner db_user/};
 
     my @files = map {"\\i $_\n"} <schema/*.sql>, @args;
-    shift @files;
 
-    (my $CMD = <<"    CMD") =~ s/^\s*//;
+    my $extensions = shift @files;
+
+    (my $script = <<"    CMD") =~ s/^\s*//;
         \\set ON_ERROR_STOP
         SET client_min_messages TO 'error';
 
@@ -53,16 +56,21 @@ sub install {
         \\set user $user
         SET client_min_messages TO 'error';
         CREATE OR REPLACE LANGUAGE plpgsql;
-        \\i schema/000-extensions.sql
-        SET SESSION AUTHORIZATION $owner;
+        $extensions;
 
+        SET SESSION AUTHORIZATION $owner;
         BEGIN;
         @files
         COMMIT;
     CMD
 
+    my @psql = ($psql);
+    unless (getpwuid($<) eq "postgres") {
+        unshift @psql, qw[su -m postgres -c];
+    }
+
     say "Creating users, database, and schema...";
-    run3 [qw[su -m postgres -c], $psql], \$CMD;
+    run3 \@psql, \$script;
 }
 
 sub upgrade {
@@ -95,8 +103,8 @@ sub upgrade {
         while (-f (my $file = "schema/upgrades/$name/${version}-to-${next}.sql")) {
             say ">>> $name: $file";
 
-            my $CMD = sprintf $template, $next, $name, $file;
-            run3 [$psql, '-q1f', '-', $database, $owner], \$CMD;
+            my $script = sprintf $template, $next, $name, $file;
+            run3 [$psql, '-q1f', '-', $database, $owner], \$script;
             exit if $?;
 
             $next = 1 + ++$version;
@@ -114,11 +122,17 @@ Gadwall::Command::schema - Install or upgrade the application's database schema
 
     Usage: ./app schema <install|upgrade> [options]
 
+        sudo ./app schema install
+        ./app schema upgrade
+
     Options:
         --psql <path>           Specify path to psql executable
 
 =head1 DESCRIPTION
 
 This command can install or upgrade the application's database schema.
+
+The "schema install" command must be run either as the postgres user, or
+as root, so as to be able to su to the postgres user without a password.
 
 =cut
